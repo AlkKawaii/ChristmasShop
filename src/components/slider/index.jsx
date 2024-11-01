e0a92fb805b89d70bfc1adc8324fb0f4840aaddb
Guilherme Alcantara <tnt.gamer00125@gmail.com>
2024-11-01 07:46:02 -0300

    fiz mais agora doq nas ultimas 2 semanas

--- src/components/slider/index.jsx
+++ src/components/slider/index.jsx
@@ -1,15 +1,33 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css'
+import { Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 import styles from './Slider.module.css';
 
 export default function Slider({ images = [] }) {
-  return <div>
-    <Swiper slidesPerView={1} >
-        <SwiperSlide><img src="https://picsum.photos/200/300?random=1" alt="imagem" /></SwiperSlide>
-        <SwiperSlide><img src="https://picsum.photos/200/300?random=2" alt="imagem" /></SwiperSlide>
-        <SwiperSlide><img src="https://picsum.photos/200/300?random=3" alt="imagem" /></SwiperSlide>
-        <SwiperSlide><img src="https://picsum.photos/200/300?random=4" alt="imagem" /></SwiperSlide>
-    </Swiper>
-  </div>;
+  return (
+    <div>
+      <Swiper
+        slidesPerView={1}
+        loop={true}
+        pagination={{ clickable: true, dynamicBullets: true }}
+        modules={[Pagination]}>
+        <SwiperSlide>
+          <img src='https://picsum.photos/200/300?random=1' alt='imagem' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src='https://picsum.photos/200/300?random=2' alt='imagem' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src='https://picsum.photos/200/300?random=3' alt='imagem' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src='https://picsum.photos/200/300?random=4' alt='imagem' />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
